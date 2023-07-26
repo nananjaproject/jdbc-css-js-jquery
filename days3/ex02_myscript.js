105a8779f1f8cbb8c82a72a315a0c3c12ba1a3b8
NANDIVE <117037428+nanandive@users.noreply.github.com>
2023-07-27 06:49:54 +0900

    js

---
+++ days3/ex02_myscript.js
@@ -0,0 +1,10 @@
+/**
+ * ex02_myscript.js
+   [외부 자바스크립트 파일 장점]
+  1. html 코딩 - js 코딩  분리
+  2. 가독성,  유지 , 보수 관리 용이
+  3. 캐시된 js 파일은 페이지 로드 속도를 향상.
+ */
+ function myAlert( name ){
+	  alert( name + "님 환영~");
+ }
